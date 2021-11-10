119a5eeaac4d823908e21aea19ee64de3e857098
Marek Wiewiorski <marek@mwicat.com>
2021-11-10 01:29:27 +0100

    Update install script

--- install_osx.sh
+++ install_osx.sh
@@ -4,11 +4,20 @@ set -o nounset
 set -o errexit
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-SCRIPT_NAME="ConsoleControlSurface"
 
 LIVE_SCRIPTS_DIR="$HOME/Music/Ableton/User Library/Remote Scripts"
 
-cd "${LIVE_SCRIPTS_DIR}"
-ln -sfn "${SCRIPT_DIR}/${SCRIPT_NAME}"
+function install_script {
+    script_name="$1"
+    source_path="${SCRIPT_DIR}/${script_name}"
+    target_path="${LIVE_SCRIPTS_DIR}/${script_name}"
+    if [ -e "$target_path" ]; then
+        echo "Target path '$target_path' already exists"
+        file "$target_path"
+        return
+    fi
+    sudo ln -sfn "$source_path" "$target_path"
+    echo "Installed into '$target_path'"
+}
 
-echo "Installed into '${LIVE_SCRIPTS_DIR}/${SCRIPT_NAME}'"
+install_script LiveOSC2
